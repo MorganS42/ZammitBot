24a90e1456d1a7b7eedd4b2898bed85dd6c8becb
Luca Murphy <z5590844@ad.unsw.edu.au>
2025-02-11 15:26:13 +1100

    it actually deploys now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import api, { route } from "@forge/api";
 
-export async function getBlogPost(payload) {
+export async function getBlogPosts(payload) {
   const response = await api.asUser().requestConfluence(route`/wiki/api/v2/labels/${payload.id}/blogposts`, {
     headers: {
       'Accept': 'application/json'
